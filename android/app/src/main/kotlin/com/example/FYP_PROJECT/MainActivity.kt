0067746b6990edc96922e593105418133fe4bb1c
Zain-ul-abdin0 <ahmza147@gmail.com>
2020-12-17 17:26:05 +0500

    Forget Password

---
+++ android/app/src/main/kotlin/com/example/FYP_PROJECT/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.FYP_PROJECT
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/AuthScreens/HeaderTabBar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:FYP_PROJECT/screen/Login.dart';
+import 'package:FYP_PROJECT/screen/SignUp.dart';
+
+class HeaderTabBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+        home: DefaultTabController(
+          length: 2,
+          child: Scaffold(
+            backgroundColor: Colors.black,
+            appBar: AppBar(
+              title: Text(
+                "Get Started",
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              backgroundColor: Colors.black,
+              bottom: PreferredSize(
+                preferredSize: Size.fromHeight(50),
+                child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Container(
+                    width: 270,
+                    child: TabBar(
+                        unselectedLabelColor: Colors.white,
+                        indicatorSize: TabBarIndicatorSize.label,
+                        indicatorColor: Color(0xFFFF0058),
+                        labelColor: Color(0xFFFF0058),
+                        tabs: [
+                          Tab(
+                            child: Text("Login"),
+                          ),
+                          Tab(
+                            child: Text("Create Account"),
+                          ),
+                        ]),
+                  ),
+                ),
+              ),
+            ),
+            body: TabBarView(
+              children: [
+                Login('Login'),
+                SignUp('SignUp'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
